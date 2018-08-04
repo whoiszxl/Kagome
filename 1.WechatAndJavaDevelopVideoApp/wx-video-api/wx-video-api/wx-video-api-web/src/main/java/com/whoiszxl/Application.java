48d73321a4eed550e6a56b71b8b4a442897fcacd
zhouxiaolong <whoiszxl@gmail.com>
2018-08-04 16:52:21 +0800

    初始化多模块的Java API项目

---
+++ 1.WechatAndJavaDevelopVideoApp/wx-video-api/wx-video-api/wx-video-api-web/src/main/java/com/whoiszxl/Application.java
@@ -0,0 +1,14 @@
+package com.whoiszxl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.whoiszxl"})
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
