dc1041c4ebeb4e45c36115db614baac0d3416274
zhouxiaolong <whoiszxl@gmail.com>
2018-08-09 15:14:15 +0800

    逆向生成pojo mapper

---
+++ 2.DistributedXLMall/xlmall-parent/xlmall-dao/src/main/java/com/whoiszxl/mapper/TbCitiesMapper.java
@@ -0,0 +1,30 @@
+package com.whoiszxl.mapper;
+
+import com.whoiszxl.pojo.TbCities;
+import com.whoiszxl.pojo.TbCitiesExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbCitiesMapper {
+    int countByExample(TbCitiesExample example);
+
+    int deleteByExample(TbCitiesExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(TbCities record);
+
+    int insertSelective(TbCities record);
+
+    List<TbCities> selectByExample(TbCitiesExample example);
+
+    TbCities selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") TbCities record, @Param("example") TbCitiesExample example);
+
+    int updateByExample(@Param("record") TbCities record, @Param("example") TbCitiesExample example);
+
+    int updateByPrimaryKeySelective(TbCities record);
+
+    int updateByPrimaryKey(TbCities record);
+}
