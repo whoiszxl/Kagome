f6d639645770e08775126ba23b990d9229470090
zhouxiaolong <whoiszxl@gmail.com>
2018-08-10 10:45:29 +0800

    提取公共controller

--- 2.DistributedXLMall/xlmall-parent/xlmall-manager-web/src/main/webapp/js/controller/brandController.js
+++ 2.DistributedXLMall/xlmall-parent/xlmall-manager-web/src/main/webapp/js/controller/brandController.js
@@ -1,25 +1,7 @@
-app.controller('brandController', function($scope, brandService) {
+app.controller('brandController', function($scope, $controller, brandService) {
 
-	// 重新调用查询品牌的接口
-	$scope.reloadList = function() {
-		// 切换页码
-		// $scope.findPage( $scope.paginationConf.currentPage,
-		// $scope.paginationConf.itemsPerPage);
-		$scope.search($scope.paginationConf.currentPage,
-				$scope.paginationConf.itemsPerPage);
-	}
-
-	// 分页控件配置
-	$scope.paginationConf = {
-		currentPage : 1,
-		totalItems : 10,
-		itemsPerPage : 10,
-		perPageOptions : [ 10, 20, 30, 40, 50 ],
-		onChange : function() {
-			$scope.reloadList();// 重新加载
-		}
-	};
-
+	$controller('baseController', {$scope:$scope}); //集成基本控制类
+	
 	$scope.findPage = function(page, size) {
 		brandService.findPage().success(function(response) {
 			$scope.list = response.rows;
@@ -52,19 +34,6 @@ app.controller('brandController', function($scope, brandService) {
 		brandService.findOne(id).success(function(response) {
 			$scope.entity = response;
 		});
-	}
-
-	// 删除时候用的，选中的ID的集合
-	$scope.selectIds = [];
-
-	// 更新复选
-	$scope.updateSelection = function($event, id) {
-		if ($event.target.checked) { // 被选中，添加到数组
-			$scope.selectIds.push(id);
-		} else {
-			var idx = $scope.selectIds.indexOf(id);// 找到这个id在数组中的位置
-			$scope.selectIds.splice(idx, 1);// 删除操作
-		}
 	}
 
 	// 批量删除
