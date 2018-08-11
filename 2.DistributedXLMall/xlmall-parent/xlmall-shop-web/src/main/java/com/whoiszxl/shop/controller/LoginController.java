2a9c02fbef3dc662366a94e873eacce6ed51e340
zhouxiaolong <whoiszxl@gmail.com>
2018-08-12 00:31:58 +0800

    实现商家的index名称显示和注销功能

---
+++ 2.DistributedXLMall/xlmall-parent/xlmall-shop-web/src/main/java/com/whoiszxl/shop/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.whoiszxl.shop.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 登录控制器
+ * @author whoiszxl
+ *
+ */
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+	
+	@GetMapping("/name")
+	public Map<String, String> name() {
+		//获取当前登录用户的用户名
+		String name = SecurityContextHolder.getContext().getAuthentication().getName();
+		Map<String, String> map = new HashMap<>();
+		map.put("loginName", name);
+		return map;
+	}
+}
---
+++ 2.DistributedXLMall/xlmall-parent/xlmall-shop-web/src/main/webapp/js/service/loginService.js
@@ -0,0 +1,9 @@
+//登录服务
+app.service("loginService", function($http) {
+	
+	//读取登录人名称
+	this.loginName=function(){
+		return $http.get('../login/name.do');		
+	}
+	
+});
