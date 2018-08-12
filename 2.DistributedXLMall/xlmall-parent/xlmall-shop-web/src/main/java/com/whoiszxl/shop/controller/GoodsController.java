e74a3d092c4bb1b1213ef39c080610a4b50b2425
zhouxiaolong <whoiszxl@gmail.com>
2018-08-13 00:55:48 +0800

    商品列表展示

--- 2.DistributedXLMall/xlmall-parent/xlmall-shop-web/src/main/java/com/whoiszxl/shop/controller/GoodsController.java
+++ 2.DistributedXLMall/xlmall-parent/xlmall-shop-web/src/main/java/com/whoiszxl/shop/controller/GoodsController.java
@@ -1,4 +1,5 @@
 package com.whoiszxl.shop.controller;
+
 import java.util.List;
 
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,6 +15,7 @@ import com.whoiszxl.sellergoods.service.GoodsService;
 
 /**
  * controller
+ * 
  * @author Administrator
  *
  */
@@ -23,36 +25,38 @@ public class GoodsController {
 
 	@Reference
 	private GoodsService goodsService;
-	
+
 	/**
 	 * 返回全部列表
+	 * 
 	 * @return
 	 */
 	@RequestMapping("/findAll")
-	public List<TbGoods> findAll(){			
+	public List<TbGoods> findAll() {
 		return goodsService.findAll();
 	}
-	
-	
+
 	/**
 	 * 返回全部列表
+	 * 
 	 * @return
 	 */
 	@RequestMapping("/findPage")
-	public PageResult  findPage(int page,int rows){			
+	public PageResult findPage(int page, int rows) {
 		return goodsService.findPage(page, rows);
 	}
-	
+
 	/**
 	 * 增加
+	 * 
 	 * @param goods
 	 * @return
 	 */
 	@RequestMapping("/add")
-	public Result add(@RequestBody Goods goods){
-		//获取到登录名
+	public Result add(@RequestBody Goods goods) {
+		// 获取到登录名
 		String sellerId = SecurityContextHolder.getContext().getAuthentication().getName();
-		//设置商家id
+		// 设置商家id
 		goods.getGoods().setSellerId(sellerId);
 		try {
 			goodsService.add(goods);
@@ -62,14 +66,15 @@ public class GoodsController {
 			return new Result(false, "增加失败");
 		}
 	}
-	
+
 	/**
 	 * 修改
+	 * 
 	 * @param goods
 	 * @return
 	 */
 	@RequestMapping("/update")
-	public Result update(@RequestBody TbGoods goods){
+	public Result update(@RequestBody TbGoods goods) {
 		try {
 			goodsService.update(goods);
 			return new Result(true, "修改成功");
@@ -77,44 +82,51 @@ public class GoodsController {
 			e.printStackTrace();
 			return new Result(false, "修改失败");
 		}
-	}	
-	
+	}
+
 	/**
 	 * 获取实体
+	 * 
 	 * @param id
 	 * @return
 	 */
 	@RequestMapping("/findOne")
-	public TbGoods findOne(Long id){
-		return goodsService.findOne(id);		
+	public TbGoods findOne(Long id) {
+		return goodsService.findOne(id);
 	}
-	
+
 	/**
 	 * 批量删除
+	 * 
 	 * @param ids
 	 * @return
 	 */
 	@RequestMapping("/delete")
-	public Result delete(Long [] ids){
+	public Result delete(Long[] ids) {
 		try {
 			goodsService.delete(ids);
-			return new Result(true, "删除成功"); 
+			return new Result(true, "删除成功");
 		} catch (Exception e) {
 			e.printStackTrace();
 			return new Result(false, "删除失败");
 		}
 	}
-	
-		/**
+
+	/**
 	 * 查询+分页
+	 * 
 	 * @param brand
 	 * @param page
 	 * @param rows
 	 * @return
 	 */
 	@RequestMapping("/search")
-	public PageResult search(@RequestBody TbGoods goods, int page, int rows  ){
-		return goodsService.findPage(goods, page, rows);		
+	public PageResult search(@RequestBody TbGoods goods, int page, int rows) {
+		// 获取商家ID
+		String sellerId = SecurityContextHolder.getContext().getAuthentication().getName();
+		// 添加查询条件
+		goods.setSellerId(sellerId);
+		return goodsService.findPage(goods, page, rows);
 	}
-	
+
 }
