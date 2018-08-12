615af0d712325dea9cd78c265122df2d975d79b5
zhouxiaolong <whoiszxl@gmail.com>
2018-08-12 22:58:00 +0800

    编辑商品页面三级分类显示

--- 2.DistributedXLMall/xlmall-parent/xlmall-shop-web/src/main/webapp/js/controller/goodsController.js
+++ 2.DistributedXLMall/xlmall-parent/xlmall-shop-web/src/main/webapp/js/controller/goodsController.js
@@ -1,6 +1,6 @@
 //控制层 
 app.controller('goodsController', function($scope, $controller, goodsService,
-		uploadService) {
+		uploadService, itemCatService) {
 
 	$controller('baseController', {
 		$scope : $scope
@@ -94,16 +94,50 @@ app.controller('goodsController', function($scope, $controller, goodsService,
 			alert("上传发生错误");
 		});
 	};
-	
 
-    $scope.entity={goods:{},goodsDesc:{itemImages:[]}};//定义页面实体结构
-    //添加图片列表
-    $scope.add_image_entity=function(){    	
-        $scope.entity.goodsDesc.itemImages.push($scope.image_entity);
-    } 
-    
-    //列表中移除图片
-    $scope.remove_image_entity=function(index){
-    	    $scope.entity.goodsDesc.itemImages.splice(index,1);
-    }
+	$scope.entity = {
+		goods : {},
+		goodsDesc : {
+			itemImages : []
+		}
+	};// 定义页面实体结构
+	// 添加图片列表
+	$scope.add_image_entity = function() {
+		$scope.entity.goodsDesc.itemImages.push($scope.image_entity);
+	}
+
+	// 列表中移除图片
+	$scope.remove_image_entity = function(index) {
+		$scope.entity.goodsDesc.itemImages.splice(index, 1);
+	}
+
+	// 读取一级分类
+	$scope.selectItemCat1List = function() {
+		itemCatService.findByParentId(0).success(function(response) {
+			$scope.itemCat1List = response;
+		});
+	}
+
+	// 读取二级分类
+	$scope.$watch('entity.goods.category1Id', function(newValue, oldValue) {
+		// 根据选择的值，查询二级分类
+		itemCatService.findByParentId(newValue).success(function(response) {
+			$scope.itemCat2List = response;
+		});
+	})
+
+	// 读取三级分类
+	$scope.$watch('entity.goods.category2Id', function(newValue, oldValue) {
+		// 根据选择的值，查询二级分类
+		itemCatService.findByParentId(newValue).success(function(response) {
+			$scope.itemCat3List = response;
+		});
+	})
+
+	// 三级分类选择后 读取模板ID
+	$scope.$watch('entity.goods.category3Id', function(newValue, oldValue) {
+		itemCatService.findOne(newValue).success(function(response) {
+			$scope.entity.goods.typeTemplateId = response.typeId; // 更新模板ID
+		});
+	})
 });
