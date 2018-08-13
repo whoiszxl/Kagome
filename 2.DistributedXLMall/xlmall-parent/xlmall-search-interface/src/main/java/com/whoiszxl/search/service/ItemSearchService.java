f5709b2f4da1504efcb6754f7ada8d10dd415d5f
zhouxiaolong <whoiszxl@gmail.com>
2018-08-13 22:56:42 +0800

    搜索功能

---
+++ 2.DistributedXLMall/xlmall-parent/xlmall-search-interface/src/main/java/com/whoiszxl/search/service/ItemSearchService.java
@@ -0,0 +1,13 @@
+package com.whoiszxl.search.service;
+
+import java.util.Map;
+
+/**
+ * 搜索服务接口
+ * @author Administrator
+ *
+ */
+public interface ItemSearchService {
+
+	public Map<String,Object> search(Map searchMap);
+}
