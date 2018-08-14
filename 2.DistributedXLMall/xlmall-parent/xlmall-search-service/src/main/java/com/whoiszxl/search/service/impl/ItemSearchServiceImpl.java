f45242efc0b73cdd9879da467d03e8be9e9b8fd1
zhouxiaolong <whoiszxl@gmail.com>
2018-08-14 19:31:59 +0800

    搜索的杂七杂八

--- 2.DistributedXLMall/xlmall-parent/xlmall-search-service/src/main/java/com/whoiszxl/search/service/impl/ItemSearchServiceImpl.java
+++ 2.DistributedXLMall/xlmall-parent/xlmall-search-service/src/main/java/com/whoiszxl/search/service/impl/ItemSearchServiceImpl.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.solr.core.SolrTemplate;
 import org.springframework.data.solr.core.query.Criteria;
@@ -128,6 +129,20 @@ public class ItemSearchServiceImpl implements ItemSearchService {
 		}
 		query.setOffset((pageNo - 1) * pageSize);// 从第几条记录查询
 		query.setRows(pageSize);
+		
+		//1.7排序
+		String sortValue= (String) searchMap.get("sort");//ASC  DESC  
+		String sortField= (String) searchMap.get("sortField");//排序字段
+		if(sortValue!=null && !sortValue.equals("")){  
+			if(sortValue.equals("ASC")){
+				Sort sort=new Sort(Sort.Direction.ASC, "item_"+sortField);
+				query.addSort(sort);
+			}
+			if(sortValue.equals("DESC")){		
+				Sort sort=new Sort(Sort.Direction.DESC, "item_"+sortField);
+				query.addSort(sort);
+			}			
+		}
 
 		// *********** 获取高亮结果集 ***********
 		// 高亮页对象
--- 2.DistributedXLMall/xlmall-parent/xlmall-search-web/src/main/webapp/js/controller/searchController.js
+++ 2.DistributedXLMall/xlmall-parent/xlmall-search-web/src/main/webapp/js/controller/searchController.js
@@ -8,15 +8,17 @@ app.controller('searchController', function($scope, searchService) {
 		'spec' : {},
 		'price' : '',
 		'pageNo' : 1,
-		'pageSize' : 40
+		'pageSize' : 40,
+		'sortField' : '',
+		'sort' : ''
 	};
 
 	// 搜索
 	$scope.search = function() {
-		$scope.searchMap.pageNo= parseInt($scope.searchMap.pageNo) ;
+		$scope.searchMap.pageNo = parseInt($scope.searchMap.pageNo);
 		searchService.search($scope.searchMap).success(function(response) {
 			$scope.resultMap = response;
-			buildPageLabel();//调用
+			buildPageLabel();// 调用
 		});
 	}
 
@@ -48,22 +50,22 @@ app.controller('searchController', function($scope, searchService) {
 		var maxPageNo = $scope.resultMap.totalPages;// 得到最后页码
 		var firstPage = 1;// 开始页码
 		var lastPage = maxPageNo;// 截止页码
-		$scope.firstDot=true;//前面有点
-		$scope.lastDot=true;//后边有点
+		$scope.firstDot = true;// 前面有点
+		$scope.lastDot = true;// 后边有点
 		if ($scope.resultMap.totalPages > 5) { // 如果总页数大于5页,显示部分页码
 			if ($scope.searchMap.pageNo <= 3) {// 如果当前页小于等于3
 				lastPage = 5; // 前5页
-				$scope.firstDot=false;//前面没点
+				$scope.firstDot = false;// 前面没点
 			} else if ($scope.searchMap.pageNo >= lastPage - 2) {// 如果当前页大于等于最大页码-2
 				firstPage = maxPageNo - 4; // 后5页
-				$scope.lastDot=false;//后边没点
+				$scope.lastDot = false;// 后边没点
 			} else { // 显示当前页为中心的5页
 				firstPage = $scope.searchMap.pageNo - 2;
 				lastPage = $scope.searchMap.pageNo + 2;
 			}
-		}else{
-			$scope.firstDot=false;//前面无点
-			$scope.lastDot=false;//后边无点
+		} else {
+			$scope.firstDot = false;// 前面无点
+			$scope.lastDot = false;// 后边无点
 		}
 		// 循环产生页码标签
 		for (var i = firstPage; i <= lastPage; i++) {
@@ -71,33 +73,48 @@ app.controller('searchController', function($scope, searchService) {
 		}
 	}
 
-	
-	//根据页码查询
-	$scope.queryByPage=function(pageNo){
-		//页码验证
-		if(pageNo<1 || pageNo>$scope.resultMap.totalPages){
+	// 根据页码查询
+	$scope.queryByPage = function(pageNo) {
+		// 页码验证
+		if (pageNo < 1 || pageNo > $scope.resultMap.totalPages) {
 			return;
-		}		
-		$scope.searchMap.pageNo=pageNo;			
+		}
+		$scope.searchMap.pageNo = pageNo;
 		$scope.search();
 	}
-	
-	
-	//判断当前页为第一页
-	$scope.isTopPage=function(){
-		if($scope.searchMap.pageNo==1){
+
+	// 判断当前页为第一页
+	$scope.isTopPage = function() {
+		if ($scope.searchMap.pageNo == 1) {
 			return true;
-		}else{
+		} else {
 			return false;
 		}
 	}
-	
-	//判断当前页是否未最后一页
-	$scope.isEndPage=function(){
-		if($scope.searchMap.pageNo==$scope.resultMap.totalPages){
+
+	// 判断当前页是否未最后一页
+	$scope.isEndPage = function() {
+		if ($scope.searchMap.pageNo == $scope.resultMap.totalPages) {
 			return true;
-		}else{
+		} else {
 			return false;
 		}
+	}
+
+	// 设置排序规则
+	$scope.sortSearch = function(sortField, sort) {
+		$scope.searchMap.sortField = sortField;
+		$scope.searchMap.sort = sort;
+		$scope.search();
+	}
+
+	// 判断关键字是不是品牌
+	$scope.keywordsIsBrand = function() {
+		for (var i = 0; i < $scope.resultMap.brandList.length; i++) {
+			if ($scope.searchMap.keywords.indexOf($scope.resultMap.brandList[i].text) >= 0) {// 如果包含
+				return true;
+			}
+		}
+		return false;
 	}
 });
