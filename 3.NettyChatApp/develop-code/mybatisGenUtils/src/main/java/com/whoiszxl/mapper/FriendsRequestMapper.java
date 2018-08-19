4cf28428690b392cc8a94c7fd0a7a5e2f34b9ce1
zhouxiaolong <whoiszxl@gmail.com>
2018-08-20 00:10:27 +0800

    逆向工程生成mybatis

---
+++ 3.NettyChatApp/develop-code/mybatisGenUtils/src/main/java/com/whoiszxl/mapper/FriendsRequestMapper.java
@@ -0,0 +1,7 @@
+package com.whoiszxl.mapper;
+
+import com.whoiszxl.pojo.FriendsRequest;
+import com.whoiszxl.utils.MyMapper;
+
+public interface FriendsRequestMapper extends MyMapper<FriendsRequest> {
+}
