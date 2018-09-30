7d7369994263cb89a3978c7f8472da9e9808f415
zhouxiaolong <whoiszxl@gmail.com>
2018-10-01 00:26:11 +0800

    创建订单没有添加更新创建时间调不通的bug

--- 4.SpringCloudSell/code/order/src/main/java/com/whoiszxl/order/service/impl/OrderServiceImpl.java
+++ 4.SpringCloudSell/code/order/src/main/java/com/whoiszxl/order/service/impl/OrderServiceImpl.java
@@ -1,13 +1,20 @@
 package com.whoiszxl.order.service.impl;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.whoiszxl.order.client.ProductClient;
+import com.whoiszxl.order.dataobject.OrderDetail;
 import com.whoiszxl.order.dataobject.OrderMaster;
+import com.whoiszxl.order.dataobject.ProductInfo;
+import com.whoiszxl.order.dto.CartDTO;
 import com.whoiszxl.order.dto.OrderDTO;
 import com.whoiszxl.order.enums.OrderStatusEnum;
 import com.whoiszxl.order.enums.PayStatusEnum;
@@ -25,22 +32,56 @@ public class OrderServiceImpl implements OrderService {
 	@Autowired
 	private OrderMasterRepository orderMasterRepository;
 	
+	@Autowired
+	private ProductClient productClient;
+	
 	@Override
 	public OrderDTO create(OrderDTO orderDTO) {
+		
+		String orderId = KeyUtil.genUniqueKey();
+		
 		//todo 2.查询商品信息(调用商品服务)
+		List<String> productIdList = orderDTO.getOrderDetailList().stream()
+			.map(OrderDetail::getProductId)
+			.collect(Collectors.toList());
+		
+		List<ProductInfo> productInfoList = productClient.getProductListByIds(productIdList);
 	    //todo 3. 计算总价
+		BigDecimal orderAmount = new BigDecimal(BigInteger.ZERO);
+		for(OrderDetail orderDetail : orderDTO.getOrderDetailList()) {
+			//单价*数量
+			for (ProductInfo productInfo : productInfoList) {
+				if(productInfo.getProductId().equals(orderDetail.getProductId())) {
+					orderAmount = productInfo.getProductPrice()
+							.multiply(new BigDecimal(orderDetail.getProductQuantity()))
+							.add(orderAmount);
+					
+					BeanUtils.copyProperties(productInfo, orderDetail);
+					orderDetail.setOrderId(orderId);
+					orderDetail.setDetailId(KeyUtil.genUniqueKey());
+					
+					//订单详情入库
+					orderDetailRepository.save(orderDetail);
+				}
+			}
+		}
+		
 	    //todo 4. 扣库存(调用商品服务)
+		List<CartDTO> cartDTOList = orderDTO.getOrderDetailList().stream()
+				.map(e -> new CartDTO(e.getProductId(), e.getProductQuantity()))
+				.collect(Collectors.toList());
+		productClient.decreaseStock(cartDTOList);
 		
 	    // 5. 订单入库
 		OrderMaster orderMaster = new OrderMaster();	
-		orderDTO.setOrderId(KeyUtil.genUniqueKey());
+		orderDTO.setOrderId(orderId);
 		BeanUtils.copyProperties(orderDTO, orderMaster);
-		orderMaster.setOrderAmount(new BigDecimal(5));
+		orderMaster.setOrderAmount(orderAmount);
 		orderMaster.setOrderStatus(OrderStatusEnum.NEW.getCode());
 		orderMaster.setPayStatus(PayStatusEnum.WAIT.getCode());
-		orderMaster.setOrderId(KeyUtil.genUniqueKey());
-		orderMaster.setCreateTime(new Date());
-		orderMaster.setUpdateTime(new Date());
+		Date now = new Date();
+		orderMaster.setUpdateTime(now);
+		orderMaster.setCreateTime(now);
 		orderMasterRepository.save(orderMaster);
 		return orderDTO;
 	}
